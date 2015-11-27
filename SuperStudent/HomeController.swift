f8a3eff0d5fa3a9d2a5151954d33b36e0bdedfa7
Logan Hollins <a00801672@dhcp-142-232-233-197.wifi.bcit.ca>
2015-11-26 19:54:46 -0800

    Added pulling announcements from mongo

--- SuperStudent/HomeController.swift
+++ SuperStudent/HomeController.swift
@@ -17,12 +17,27 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var tableView2: UITableView!
     @IBOutlet weak var bcitLogo: UIImageView!
     let bcit = "bcit.jpg"
-    let announcements = ["iOS cancelled, D'Arcy umemployed", "D'Arcy elected as Prime Minister of Canada", "Canada to build national information sharing iOS app", "Logan Hollins wins the spanish lottery"]
+    var announcements:[String] = []
     let events = ["Shane's Birthday Bonanza", "AMD event", "National Paint Drying Observation Challenge", "Solar Eclipse 2016", "Snape kills Dumbledore"]
     let simpleTableIdentifier = "SimpleTableIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Alamofire.request(.GET, "https://api.mongolab.com/api/1/databases/rhythmictracks/collections/Announcements?apiKey=L4HrujTTG-XOamCKvRJp5RwYMpoJ6xCZ").responseJSON { response in
+            
+            if let json = response.result.value {
+                var data = JSON(json)
+                for i in 0...data.count - 1 {
+                    self.announcements.append(String(data[i]["title"]))
+                }
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.tableView.reloadData()
+                })
+                
+
+            }
+        }
+
         bcitLogo.image = UIImage(named: bcit)
     }
     
