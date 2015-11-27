d6613369f7bdd71d8a2fd3e022ae2b26aa57639a
Logan Hollins <a00801672@dhcp-142-232-233-129.wifi.bcit.ca>
2015-11-27 13:39:54 -0800

    Added loading student info from mongo

--- SuperStudent/SettingsController.swift
+++ SuperStudent/SettingsController.swift
@@ -7,21 +7,36 @@
 //
 
 import UIKit
-
+import Alamofire
+import SwiftyJSON
 class SettingsController: UIViewController {
     
-    //    let jimbo = "jimbo.jpeg"
-    //    let mirela = "mirela.jpeg"
-    //    let darcy = "darcy.png"
+    @IBOutlet weak var aboutText: UITextView!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var mirelaView: UIImageView!
     @IBOutlet weak var darcyView: UIImageView!
     @IBOutlet weak var profilePic: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        profilePic.image = UIImage(named: "jimbo")
-        //        mirelaView.image = UIImage(named: "mirela")
-        //        darcyView.image = UIImage(named: "darcy")
+        Alamofire.request(.GET, "https://api.mongolab.com/api/1/databases/rhythmictracks/collections/Users", parameters: ["apiKey": "L4HrujTTG-XOamCKvRJp5RwYMpoJ6xCZ", "q": "{'studentId': '\(StudentInfo.StudentId)'}"]).responseJSON { response in
+            
+            if let json = response.result.value {
+                var data = JSON(json)
+                print(data)
+                let fname = data[0]["firstName"].stringValue
+                let lname = data[0]["lastName"].stringValue
+                self.nameLabel.text = "\(fname) \(lname)"
+                self.idLabel.text = data[0]["studentId"].stringValue
+                self.emailLabel.text = data[0]["email"].stringValue
+                self.aboutText.text = data[0]["about"].stringValue
+                self.profilePic.image = UIImage(named: data[0]["pic"].stringValue)
+                
+            }
+        }
+
     }
     
     override func didReceiveMemoryWarning() {
